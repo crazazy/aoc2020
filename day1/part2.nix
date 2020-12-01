f972a28961b019d2dfa48cf3cb3a9fa130f6459e
Erik Oosting <erik99.oosting@gmail.com>
2020-12-01 15:55:15 +0100

    added part 2 of day 1

---
+++ day1/part2.nix
@@ -0,0 +1,17 @@
+{ input ? import <input> }:
+# to run, use `nix eval -f ./part2.nix -I 'input=./input.nix' output`
+let
+  inherit (builtins) elem elemAt foldl' head map filter tail;
+  flatten = xs: foldl' (a: b: a ++ b) [] xs;
+  fix = f: let x = f x; in x;
+  sorted = builtins.sort builtins.lessThan input;
+  quickElem = f: xs: let i = elemAt xs; in f i;
+  allPairs = flatten (map (x: map (y: [x y]) sorted) sorted);
+  positives = filter (quickElem (i: i 0 > i 1)) allPairs;
+  positives2 = filter (quickElem (i: i 0 + i 1 < 2020)) allPairs;
+  allTriplets = flatten (map (x: map (y: x ++ [y]) sorted) positives2);
+  triplets = filter (quickElem (i: (i 0 + i 1 + i 2) == 2020)) allTriplets;
+  output = quickElem (i: i 0 * i 1 * i 2) (head triplets);
+in
+  { inherit allPairs triplets output; }
+
