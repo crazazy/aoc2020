2d84ca124d5966d14a76c9a0f88609c08b6b60b4
Erik Oosting <erik99.oosting@gmail.com>
2020-12-01 13:56:35 +0100

    finished day 1 of AoC

---
+++ day1/default.nix
@@ -0,0 +1,10 @@
+# use nix eval -f ./default.nix output to view output
+let
+  inherit (builtins) elem elemAt filter length map toString toFile;
+  input = import ./input.nix;
+  negatives = map (x: 2020 - x) input;
+  pair = filter (x: elem x negatives) input;
+  output = elemAt pair 0 * elemAt pair 1;
+in
+  { inherit output pair; }
+
