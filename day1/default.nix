2932599ed384b866554ea4bdf7947cdffe313fe6
Erik Oosting <erik99.oosting@gmail.com>
2020-12-01 14:05:36 +0100

    use NIX_PATH instead of hardcoded path in solutions

--- day1/default.nix
+++ day1/default.nix
@@ -1,7 +1,7 @@
-# use nix eval -f ./default.nix output to view output
+# use `nix eval -f ./default.nix -I 'input=<input file>' output` to view output
 let
   inherit (builtins) elem elemAt filter length map toString toFile;
-  input = import ./input.nix;
+  input = import <input>;
   negatives = map (x: 2020 - x) input;
   pair = filter (x: elem x negatives) input;
   output = elemAt pair 0 * elemAt pair 1;
